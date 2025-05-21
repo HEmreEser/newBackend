bde03532252b9bacf6f58b4254791d206e0303db
HEmreEser <119568554+HEmreEser@users.noreply.github.com>
2025-05-21 19:45:02 +0200

    Add files via upload

--- main/java/edu/hm/cs/kreisel_backend/dto/AuthRequestDto.java
+++ main/java/edu/hm/cs/kreisel_backend/dto/AuthRequestDto.java
@@ -3,9 +3,14 @@
 package edu.hm.cs.kreisel_backend.dto;
 
 import lombok.Data;
-
 @Data
 public class AuthRequestDto {
     private String email;
-    private String password; // For future password implementation
+    private String password;
+
+    // Konstruktor für Lombok ergänzen
+    public AuthRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
